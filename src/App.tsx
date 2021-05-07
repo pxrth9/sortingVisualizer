c6564f88f963c5e27c84e9fb27ab9a488b955d9a
Parth Patel <ppatil91099@gmail.com>
2021-05-07 13:03:16 -0400

    testing block

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnin' React
+          Learn React
         </a>
       </header>
     </div>
